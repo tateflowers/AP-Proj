559bde0eb11bc3146d47251700d2c4e46ff87d6a
Ned27 <eng27@students.stab.org>
2025-02-02 19:51:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var url ="https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Animals/100%20Birds%20of%20the%20World.csv"
+var names = getColumn(url,1)
+var scientificNames = getColumn(url,2)
+var conservationStatuses = getColumn(url,3)
+var primaryColors = getColumn(url,4)
+var diets = getColumn(url,5)
+
+function searchBirds(primaryColor, diet){
+    var matchingBirds=[];
+        for(var i=0; i<diets.length;i++){
+            if((diets[i].toLowerCase().includes(diet.toLowerCase()))){}
+        }
+}
