7b0def9f3470f8b97263ac26f9d18666cb0454be
tateflowers <tflowers28@students.stab.org>
2025-02-02 20:04:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var url ="https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Animals/100%20Birds%20of%20the%20World.csv"
+console.log("Hi")
+var url ="https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Animals/100%20Birds%20of%20the%20World.csv";
 var names = getColumn(url,1)
 var scientificNames = getColumn(url,2)
 var conservationStatuses = getColumn(url,3)
@@ -6,15 +7,17 @@ var primaryColors = getColumn(url,4)
 var diets = getColumn(url,5)
 
 function searchBirds(primaryColor, diet){
+    
     var matchingBirds=[];
-        for(var i=0; i<diets.length;i++){
-            if((diets[i].toLowerCase().includes(diet.toLowerCase()))&&(primaryColors.toLowerCase()[i]==primaryColor.toLowerCase())){
-                matchingBirds.push(names[i]);
+        for(var i=0; i< diets.length;i++){
+            if((diets[i].toLowerCase().includes(diet.toLowerCase()))&&(primaryColors[i].toLowerCase().includes(primaryColor.toLowerCase()))){
+                matchingBirds.push(names[i])
             }
+            if(matchingBirds.length == 0){
+                return "No birds found"
+            }
+            
         }
-    if(matchingBirds.length==0){
-        return "No birds found."
-    }
-    return matchingBirds
+        return matchingBirds;
 }
-console.log
+console.log(searchBirds("blue","seeds"))
