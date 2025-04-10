c235b0076efa634afc570e78e8b8ceb16268d234
tateflowers <tflowers28@students.stab.org>
2025-04-10 09:58:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var url ="https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Animals/100%20Birds%20of%20the%20World.csv";
 var names = getColumn(url,1)
 var scientificNames = getColumn(url,2)
@@ -6,19 +5,20 @@ var conservationStatuses = getColumn(url,3)
 var primaryColors = getColumn(url,4)
 var diets = getColumn(url,5)
 
+
 function searchBirds(primaryColor, diet){
-    
+
     var matchingBirds=[];
         for(var i=0; i < names.length; i++){
             if(diets[i].toLowerCase().includes(diet.toLowerCase())&&primaryColors[i].toLowerCase().includes(primaryColor.toLowerCase())){
                 matchingBirds.push(names[i])
             }
         }
-        if(matchingBirds.length == 0){
-            return "not found yo"
+            if(matchingBirds.length == 0){
+                document.getElementById("output").innerHTML = "No birds found with these inputs";
         }
-        else{
-        return matchingBirds;
+            else{
+                document.getElementById("output").innerHTML = matchingBirds;
         }
 }
-console.log(searchBirds("brown","seeds"));
+searchBirds();
